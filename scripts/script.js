9e22a8310f9e60f63e64710d6b29a98fc008bbb2
Seung <imoron@crackthecode.la>
2022-11-15 20:11:57 -0500

    Agregando mis archivos html y css

---
+++ scripts/script.js
@@ -0,0 +1,39 @@
+    //Variables
+    //Condicionales
+    //Bucles
+
+
+let Nombre = "Ivana"
+let  Apellido= "M.Rios"
+let  Peso= "47.1"
+let  Altura= "1,57"
+let  Edad= "10.10"
+
+console.log(Nombre)
+console.log(Apellido)
+console.log(Altura)
+console.log(Peso)
+console.log(Edad)
+
+//Operaciones matematicas
+let a = 40
+let b = 80
+let c = 160
+
+//let resultado1=
+//let resultado2=
+//let resultado3=
+//let resultado4=
+
+resultado1 = (a + b) * 6
+resultado2 = c / (a  + 2)
+resultado3 = (a - b) + 9
+resultado4 = b + c * a /b * 7
+
+console.log(resultado1)
+console.log(resultado2)
+console.log(resultado3)
+console.log(resultado4)
+
+
+4%2
